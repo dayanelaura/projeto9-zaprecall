892559450458bfa340496f64a7fbd7cff4461c41
Dayane <dayanelaura3@gmail.com>
2022-10-11 01:43:07 -0300

    personalização do deck just for kidding with my friend's group

--- src/mock.js
+++ src/mock.js
@@ -1,4 +1,49 @@
 const DECK = [
+  { pergunta: "Matt",
+    resposta: "Não sabotar minha felicidade ficando apx por comprometidas",
+  },
+  { pergunta: "Joyce",
+    resposta: "Passar pelo final de semana sem mandar mensagem para ex",
+  },
+  { pergunta: "Mari",
+    resposta: "Não trocar os migos do Moon Gang pelo B41xar14 Bléh",
+  },
+  { pergunta: "André",
+    resposta: "Nunca deixar a Joy no vácuo pq ela fica pistola </3",
+  },
+  { pergunta: "Apolinario",
+    resposta: "Não esquecer meu guarda-chuva para a festa",
+  },
+  { pergunta: "Chris",
+    resposta: "Passar 1 dia sem arranjar treta",
+  },
+  { pergunta: "Mari",
+    resposta: "Ajudar a Day nos projetos de React"
+  },
+  { pergunta: "Matt",
+    resposta: "é aNNya",
+  },
+  { pergunta: "Gabriel",
+    resposta: "Exercitar o perdão S2 *abraço coletivo*",
+  },
+  { pergunta: "Abdullay",
+    resposta: "Não é gaRRota, é gaRota haha",
+  },
+  { pergunta: "Annya",
+    resposta: "Comprar minha passagem para o Brasil",
+  },
+  { pergunta: "Apolinario",
+    resposta: "No smoking pendrive",
+  },
+  { pergunta: "Chris",
+    resposta: "Assistir Friends",
+  },
+  { pergunta: "Gabriel",
+    resposta: "Lançar música nova para os meus fãs",
+  },
+]
+
+/* const DECK = [
     { pergunta: "O que é JSX?", 
       resposta: "Uma extensão de linguagem do JavaScript",
     },
@@ -23,6 +68,6 @@ const DECK = [
     { pergunta: "Usamos estado (state) para __",
       resposta: "Dizer para o React quais informações quando atualizadas devem renderizar a tela novamente",
     }
-]
+] */
 
 export default DECK;
